ce7f68ee307691f275e04e19a73ccc7261bcb0e8
jackying <yjc_com@163.com>
2019-01-17 21:31:17 +0800

    no message

--- static/h-ui/js/H-ui.js
+++ static/h-ui/js/H-ui.js
@@ -8802,7 +8802,7 @@ function($) {
   };
   $.fn.datetimepicker.Constructor = Datetimepicker;
   var dates = $.fn.datetimepicker.dates = {
-    'zh-CN': {
+    'zh-cn': {
       days:        ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"],
       daysShort:   ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
       daysMin:     ["日", "一", "二", "三", "四", "五", "六", "日"],
